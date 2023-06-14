385640f10bead1138b1be6a185ec29f0f62655ab
Lanaanvar <lana.anvar03@gmail.com>
2023-06-14 19:08:19 +0530

    First java project

--- Project1/src/main/java/Add.java
+++ Project1/src/main/java/Add.java
@@ -1,15 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package add;
 
 /**
  *
  * @author lanaa
  */
 public class Add {
-
     public int x;
     public int y;
     
@@ -32,16 +30,6 @@ public class Add {
     public int sum()
     {
         return x+y;
-    }
-    public static void main(String[] args) {
-        // TODO code application logic here
-        Add obj=new Add();
-        obj.setx(15);
-        obj.sety(35);
-        obj.getx();
-        obj.gety();
-
-        System.out.println(obj.sum());
     }
     
 }
